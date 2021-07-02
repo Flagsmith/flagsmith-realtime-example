9172f0a05861ee1acfbf814c695936458b111515
kyle-ssg <kyle@solidstategroup.com>
2021-07-02 18:10:28 +0100

    Pusher script

--- pages/api/message.js
+++ pages/api/message.js
@@ -1,11 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const Pusher = require("pusher");
-const pusher = new Pusher(JSON.parse(process.env.pusher));
+// const Pusher = require("pusher");
+// const pusher = new Pusher(JSON.parse(process.env.pusher));
 
 export default function handler(req, res) {
-  pusher.trigger("my-channel", "my-event", {
-    message: "hello world"
-  });
+  // pusher.trigger("my-channel", "my-event", {
+  //   message: "hello world"
+  // });
+  console.log(process.env.pusher)
   res.status(200).json({ name: 'John Doe' })
 }
