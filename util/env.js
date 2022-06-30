d728dae97ecf548861e706a57485cdb806061659
kyle-ssg <kyle@solidstategroup.com>
2022-06-30 07:43:16 +0100

    Update pusher

--- util/env.js
+++ util/env.js
@@ -1,5 +1,5 @@
 export default {
     flagsmithAPI:"https://edge.api.flagsmith.com/api/v1/",
     flagsmith:"VmyxnCfVjyrrRZZTt8pD95",
-    pusher:"7c73f13d1816f4f40cce"
+    pusher:"2b1312b7b73941b960ba"
 }
